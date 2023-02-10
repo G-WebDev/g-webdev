ae286cc55433137888005dbe8a9ae7e323d97e0f
G. Ellegeest <gwen.ellegeest@outlook.com>
2023-02-10 22:41:43 +0100

    yoo

--- g-webdev/src/components/navbar/Navbar.jsx
+++ g-webdev/src/components/navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 // IMPORT INTERN STYLE
 import Style from "./NavBar.module.css";
 
+// Yooooo
+
 const NavBar = () => {
   return (
     <div className={Style.navbar}>
