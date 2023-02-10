b14dae79ba948cdd3511a1be102c1e8fb5e61be5
Gwen1985 <gwen.ellegeest@outlook.com>
2023-02-10 02:23:14 +0100

    Set BrowserRouter and sub pages

--- g-webdev/src/App.js
+++ g-webdev/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
+const Home = () => <h1>Home</h1>;
+const Services = () => <h1>Services</h1>;
+const Portfolio = () => <h1>Portfolio</h1>;
+const Contact = () => <h1>Contact</h1>;
+
+const App = () => {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
 
-    </div>
+      <Route exact path="/" component={Home} />
+      <Route path="/services" component={Services} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
-}
+};
 
 export default App;
