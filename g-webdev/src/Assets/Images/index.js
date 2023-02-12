802fd8e820ec3ae163d6174283704666291bc515
Gwen1985 <gwen.ellegeest@outlook.com>
2023-02-12 16:32:06 +0100

    update home page

--- g-webdev/src/Assets/Images/index.js
+++ g-webdev/src/Assets/Images/index.js
@@ -1,10 +1,12 @@
 import Logo from "./Logo.png";
 import LogoWeb3 from './LogoWeb3.png';
 import Web3 from './Web3.png';
-import Web3_banner from './Web3_banner.png'
-import Intro_1 from './intro_1.png'
-import HomeBg from './homeBg.png'
-import BgImage from './bg_image.jpg'
+import Web3_banner from './Web3_banner.png';
+import Intro_1 from './intro_1.png';
+import HomeBg from './homeBg.png';
+import BgImage from './bg_image.jpg';
+import LaptopFrame from './LaptopFrame.png';
+import LaptopFrame2 from './LaptopFrame2.png';
 
 
-export default {Logo, LogoWeb3, Web3, Web3_banner, Intro_1, HomeBg, BgImage}
+export default { Logo, LogoWeb3, Web3, Web3_banner, Intro_1, HomeBg, BgImage, LaptopFrame, LaptopFrame2 }
--- g-webdev/src/Pages/Home/Home.jsx
+++ g-webdev/src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Typed from "typed.js";
 // IMPORT INTERN STYLE
 import Style from "./Home.module.css";
 
-// import images from "../../Assets/Images";
+import images from "../../Assets/Images";
 
 const Home = () => {
 
@@ -15,9 +15,9 @@ const Home = () => {
   useEffect(() => {
     const options = {
       strings: [
-        'webdesign...'
+        'Web development...'
       ],
-      typeSpeed: 150
+      typeSpeed: 70,
     };
 
     // elRef refers to the <span> rendered below
@@ -33,6 +33,7 @@ const Home = () => {
   return (
     <div className={Style.home}>
       <div className={Style.home__container}>
+
         <div className={Style.home__intro__box}>
 
           <div className={Style.home__intro__title__box}>
@@ -42,15 +43,53 @@ const Home = () => {
           </div>
 
           <div className={Style.home__intro__text__box}>
-            <div className={Style.home__intro__text}>Een website nodig? Of zoek je naar een webdesigner in Gent?
-              Bij G.WebDev ontwerpen, bouwen, onderhouden en optimaliseren we websites voor alle branches.
-              G.WebDev maakt dé website, webshop of webapplicatie die jouw online activiteit naar een hoger niveau brengt.</div>
+            <div className={Style.home__intro__text}>Een website nodig? Of zoek je naar een web developer in Gent? <br />
+              <strong>G.WebDev</strong> ontwerpt, bouwt, onderhoud en optimaliseerd uw dé website, webshop of webapplicatie die jouw online activiteit naar een hoger niveau brengt.<br />
+
+            </div>
+          </div>
+
+          <div className={Style.home__intro__box__image}>
+            <img src={images.HomeBg} className={Style.home__laptop} />
           </div>
 
         </div>
       </div>
+      <div className={Style.home__content__box}>
 
-    </div>
+        <div className={Style.home__content__block1}>
+          <div className={Style.home__content__block__title}>
+            WEBSITE LATEN MAKEN?
+          </div>
+          <div className={Style.home__content__block__text}>
+            Een website laten maken is de eerste stap naar succes voor uw bedrijf.
+            De taak lijkt misschien een beetje ontmoedigend, maar G.WebDev helpt en begeleidt je tijdens het hele proces.
+          </div>
+        </div>
+
+        <div className={Style.home__content__block2}>
+          <div className={Style.home__content__block__title}>
+            OVER G.WebDev.
+          </div>
+          <div className={Style.home__content__block__text}>
+            Zoek je een webdesign bureau in Gent? G.WebDev is een web developer met passie voor het web.
+            Met jaren ervaring en de goede service bouwen we voor jou als ambitieus bedrijf een krachtige website!
+          </div>
+        </div>
+
+        <div className={Style.home__content__block3}>
+          <div className={Style.home__content__block__title}>
+            WEBDDEVELOPER IN GENT.
+          </div>
+          <div className={Style.home__content__block__text}>
+            Begin vandaag nog met het digitaliseren van je onderneming. Je online aanwezigheid is onmisbaar.
+            Neem daarom plaats op het internet en bereik meer klanten, want zij zoeken online.
+          </div>
+        </div>
+      </div>
+
+
+    </div >
   );
 };
 
